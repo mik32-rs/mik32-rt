5f26903b889981301ab9335df591459874d937e7
RaulIQ <raulalimbekov@gmail.com>
2025-09-15 15:15:19 +0300

    pass  cargo test

--- src/asm.rs
+++ src/asm.rs
@@ -1,3 +1,5 @@
+#![cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
+
 use core::arch::global_asm;
 
 global_asm!(
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![no_main]
 
+#[cfg(any(target_arch = "riscv32", target_arch = "riscv64"))]
 mod asm;
 pub use mik32_rt_macros::*;
